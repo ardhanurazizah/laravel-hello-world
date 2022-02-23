d18fd78a85cbf51945b6e50b62524aa2a6be5a2e
ardhanurazizah <naardha0@gmail.com>
2022-02-22 22:18:39 -0800

    Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+//Praktikum 1
+//1
+Route::get('/', function () {
+    echo "Hi! Selamat Datang di Website Laravel";
+   });
+
+//2
+Route::get('/about', function () {
+    echo "Nama  : Ardha Nur Azizah <br>";
+    echo "NIM   : 2041720038 <br>";
+    echo "Kelas : TI-2G <br>";
+   });
+
+//3
+Route::get('/articles/{id}', function ($id){
+    echo "Ini adalah halaman Artikel dengan ID: ".$id;
 });
